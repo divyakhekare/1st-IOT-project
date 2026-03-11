45aa9629b1dabefbeb546f0be5438a1cba303b0e
divyakhekare <khekaredivya12082006@gmail.com>
2026-03-11 19:24:56 +0530

    Create LED_BLINK.ino

---
+++ LED_BLINK.ino
@@ -0,0 +1,23 @@
+//this is my first iot program
+
+#define my_led 7 
+// global declaration, LED pin no.7
+
+void setup() {
+
+// put your setup code here, to run once:
+
+pinMode (my_led, OUTPUT);
+
+void loop() {
+// put your main code here, to run repeatedly:
+
+digitalWrite(my_led, HIGH);
+
+delay(1000);
+
+digitalWrite(my_led, LOW);
+
+delay(1000);
+
+}
