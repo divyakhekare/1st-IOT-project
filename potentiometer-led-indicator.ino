06884da3c85fa0770560ad72d4b9ce6b9c145948
divyakhekare <khekaredivya12082006@gmail.com>
2026-04-13 13:11:04 +0530

    potentiometer-led-indicator

--- potentiometer-led-indicator.ino
+++ potentiometer-led-indicator.ino
@@ -1,1 +1,26 @@
-.
+#define pot A5
+#define ledgreen 7
+#define ledred 2
+
+void setup() {
+  
+Serial.begin(9600);
+pinMode(pot,INPUT); //pin type
+}
+
+void loop() {
+     int x = analogRead(pot);
+    // Serial.print("POT value");
+     Serial.println(x);
+      
+      if(x>100){
+          digitalWrite(ledgreen,HIGH);
+          digitalWrite(ledred,LOW);
+      }
+      else{
+          digitalWrite(ledgreen,LOW);
+          digitalWrite(ledred,HIGH);
+      }
+      delay(1000);
+          
+}
