0942a833714de1d6038ed235cca99468190a12eb
divyakhekare <khekaredivya12082006@gmail.com>
2026-04-17 20:34:20 +0530

    Update LED_GLOW.ino

--- LED_GLOW.ino
+++ LED_GLOW.ino
@@ -1,15 +1,13 @@
-//this is my first iot program
-
-#define my_led 7
- // global declaration, LED pin no.7
+#define my_led 7 
 
 void setup() {
 
+pinMode (my_led, OUTPUT);
 
-  pinMode (my_led, OUTPUT);
 }
 
-   void loop() {
+void loop() {
 
-   digitalWrite(my_led, HIGH);
+digitalWrite(my_led, HIGH);
+ 
 }
